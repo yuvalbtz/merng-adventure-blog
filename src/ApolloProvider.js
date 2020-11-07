ddb2263e4a6cbd761a245df2593cd2d7c700d3e3
yuvalbtz <yuvalbentzur1996@gmail.com>
2020-11-07 23:04:38 +0200

    edited proxy and base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/'
+  uri: 'https://intense-lowlands-40733.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
