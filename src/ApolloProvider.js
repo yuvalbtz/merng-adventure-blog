e464e28d9d7119044d3dcac7850db97a0b01c629
Yuval Ben Tzur <64732709+yuvalbtz@users.noreply.github.com>
2023-03-09 19:54:42 +0200

    Update ApolloProvider.js

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://blog-adventure.onrender.com/'
+  uri: 'https://blog-adventure.onrender.com'
 });
 
 const authLink = setContext(() => {
